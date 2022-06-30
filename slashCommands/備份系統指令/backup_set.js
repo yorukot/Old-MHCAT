8ecaf8fea085eb2f42d51ee61a144d998d64699b
bengod1020 <ben.hrc57@gmail.com>
2022-06-30 20:27:53 +0800

    Add existing file

--- slashCommands/備份系統指令/backup_set.js
+++ slashCommands/備份系統指令/backup_set.js
@@ -20,7 +20,11 @@ module.exports = {
     emoji: `<:configuration:985943474786414722>`,
     run: async (client, interaction, options) => {
         await interaction.deferReply().catch(e => { });
-        function errors(content){const embed = new MessageEmbed().setTitle(`<a:error:980086028113182730> | ${content}`).setColor("RED");interaction.followUp({embeds: [embed],ephemeral: true})}
+        const lodding = new MessageEmbed().setTitle("<a:load:986319593444352071> | 我正在玩命幫你備份(根據伺服器大小約需要1-20秒)!").setColor("GREEN")
+                const lodding_msg = await interaction.followUp({
+                    embeds: [lodding]
+                })
+        function errors(content){const embed = new MessageEmbed().setTitle(`<a:error:980086028113182730> | ${content}`).setColor("RED");lodding_msg.edit({embeds: [embed],ephemeral: true})}
         if(!interaction.member.permissions.has(Permissions.FLAGS.ADMINISTRATOR))return errors("你必須擁有\`管理者\`才能使用")
         var fs = require('fs');
         var dir = __dirname+"/backups/"+`${interaction.guild.id}`;
@@ -29,17 +33,13 @@ module.exports = {
         }
         backup.setStorageFolder(__dirname+"/backups/"+`${interaction.guild.id}`);
         backup.list().then((backups) => {
-            if(backups.length > 4)return errors("最高只能記錄\`4\`次備份!請使用\`/備份刪除 備份id\`")
-            const lodding = new MessageEmbed().setTitle("<a:load:986319593444352071> | 我正在玩命幫你備份(根據伺服器大小約需要1-20秒)!").setColor("GREEN")
-                const lodding_msg = await interaction.followUp({
-                    embeds: [lodding]
-                })
+            if(backups.length >= 2)return errors("最高只能記錄\`2\`次備份!請使用\`/備份刪除 備份id:\`")
             backup.create(interaction.guild, {
-                maxMessagesPerChannel: 200,
+                maxMessagesPerChannel: 10,
                 jsonBeautify: true,
                 saveImages: "base64"
             }).then((backupData) => {
-                lodding_msg.update({embeds: [
+                lodding_msg.edit({embeds: [
                     new MessageEmbed()
                     .setTitle(`<a:greentick:980496858445135893> | 成功備份，你的備份id為\`${backupData.id}\`使用\`/加載備份進行還原 id:${backupData.id}\``)
                     .setColor("GREEN")

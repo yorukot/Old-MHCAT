dd0385196487e631503e6748ff88bc9106d63d06
Night Cat <64583797+bengod1020@users.noreply.github.com>
2023-03-14 01:12:37 +0800

    新增ping

--- slashCommands/實用工具/ping.js
+++ slashCommands/實用工具/ping.js
@@ -16,7 +16,7 @@ const {
 } = require('discord.js');
 module.exports = {
     name: 'ping',
-    cooldown: 10,
+    cooldown: 0,
     description: '查看我的ping',
     //video: 'https://mhcat.xyz/commands/announcement.html',
     emoji: `<:icons_goodping:1084881470075703367>`,
@@ -24,7 +24,7 @@ module.exports = {
         try {
             console.log(interaction.createdTimestamp)
             console.log(Date.now())
-            interaction.reply(`${Date.now() - interaction.createdTimestamp > 100 ? Date.now() - interaction.createdTimestamp > 200 ? "<:icons_badping:1084881519581069482>"  : "<:icons_idelping:1084881570013388860>" : "<:icons_goodping:1084881470075703367>"} **Pong!** \`${Date.now() - interaction.createdTimestamp}\``)
+            interaction.reply(`${Date.now() - interaction.createdTimestamp > 150 ? Date.now() - interaction.createdTimestamp > 225 ? "<:icons_badping:1084881519581069482>"  : "<:icons_idelping:1084881570013388860>" : "<:icons_goodping:1084881470075703367>"} **Pong!** \`${Date.now() - interaction.createdTimestamp}\``)
         } catch (error) {
             const error_send = require('../../functions/error_send.js')
             error_send(error, interaction)

017a2fa365a24a0f5213d5f95f2095c4ad1b0186
Night Cat <64583797+bengod1020@users.noreply.github.com>
2023-03-15 02:41:47 +0800

    修復bug

--- slashCommands/管理系統/create_logging.js
+++ slashCommands/管理系統/create_logging.js
@@ -65,7 +65,7 @@ module.exports = {
                 })
             }
             if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) return errors(`你需要有\`${perms}\`才能使用此指令`)
-            const channel = interaction.options.getChannel("Channel")
+            const channel = interaction.options.getChannel("channel")
             const channel_id = channel.id
             const loggin_embed = new EmbedBuilder()
                 .setTitle("<:logfile:985948561625710663> 日誌系統")
